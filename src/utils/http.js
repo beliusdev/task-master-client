73e22de60bd8c6da0459ed21878d0ced83454344
beliusdev <121978627+beliusdev@users.noreply.github.com>
2024-10-26 11:28:55 +0400

    Changed base_url

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BASE_URL = 'https://task-master-ephr.onrender.com';
+export const BASE_URL = 'task-master-server-production.up.railway.app';
 
 export default function http(withToken) {
   const token = localStorage.getItem('tm-token');
